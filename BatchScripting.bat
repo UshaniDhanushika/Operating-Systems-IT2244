36643524d6398c3e25d2ec7ad9f72e083cd9628a
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-03-15 18:27:06 +0530

    Batch Scripting
    This batch script organizes folders, creating main directories (Critera_1 to Critera_6) with structured subfolders (Standard_1, Standard_2, etc.) under each. It keeps things well-organized.
    
    cd (Change Directory): Moves into a specified folder.
         Ex: cd Critera_1 → Moves into the Critera_1 directory.
    
    cd.. (Move Up a Directory): Moves back to the parent directory.   
        Ex: inside Critera_1, running cd.. returns to the previous folder.

---
+++ BatchScripting.bat
@@ -0,0 +1,28 @@
+mkdir Critera_1 
+cd Critera_1 
+	mkdir Standard_1
+	mkdir Standard_2
+	mkdir Standard_3
+	mkdir Standard_4
+cd..
+mkdir Critera_2
+cd Critera_2
+	mkdir Standard_1
+	mkdir Standard_2
+cd..
+mkdir Critera_3 
+cd Critera_3 
+	mkdir Standard_1
+cd..
+mkdir Critera_4 
+cd Critera_4 
+	mkdir Standard_1
+	mkdir Standard_2
+	mkdir Standard_3
+cd..
+mkdir Critera_5 
+cd Critera_5
+	mkdir Standard_1
+	mkdir Standard_2
+cd..
+mkdir Critera_6 
