4bd4b1e8665d9cbf637b017cd06e7f6efa35281c
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-04-28 22:13:07 +0530

    Add files via upload

---
+++ Day 01 2025-03-14/Codes/Print.bat
@@ -0,0 +1,14 @@
+@echo off
+  :: Turns off command display in the terminal.
+  
+echo Username : %USERNAME%
+  :: " echo " is used to prints text on the screen.
+  :: " %USERNAME% " is used to displays the username
+  
+echo Windows Version : 
+
+ver 
+:: " ver " used to displays the current Windows version
+
+pause
+::stops the script and waits for user input (pressing any key)
