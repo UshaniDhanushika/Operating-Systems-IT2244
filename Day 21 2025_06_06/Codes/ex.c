7e7e1f11c4633df76a949df91e6d3dd62825e751
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-06-11 14:23:48 +0530

    Add files via upload

---
+++ Day 21 2025_06_06/Codes/ex.c
@@ -0,0 +1,14 @@
+/* Exercise
+
+1. read two input from user ( //Parent Process )
+	name:
+	reg No:
+	age:
+	
+2. print the output from the child ( // child process )
+	name: abc
+	reg No: 2021/ICT/01
+	age: 25
+	
+*/
+
